a60652bd22cd7860fe673b63d7801f13b303bf9f
Manasseh <kimmanasseh14@gmail.com>
2023-03-28 21:28:55 +0300

    add text to readme

--- forward_kinematics_mine/forward_kinematics.cpp
+++ forward_kinematics_mine/forward_kinematics.cpp
@@ -9,60 +9,70 @@ using namespace Eigen;
 
 int main()
 {
-    MatrixXd T (4,4), T1 (4,4), T2 (4,4), T3(4,4), T4(4,4); //the overall transformation matrix and t1...t4 matrices
+    MatrixXd T (4,4);
+    MatrixXd T1 (4,4);
+    MatrixXd T2 (4,4);
+    MatrixXd T3(4,4);
+    MatrixXd T4(4,4); //the overall transformation matrix and t1...t4 matrices
 
     // link length for t1...t4
-    double linkLengthT1 {0};
-    double linkLengthT2 {0.130};
-    double linkLengthT3 {0.135};
-    double linkLengthT4 {0.126};
+    double linkLengthT1 {0.3};
+    double linkLengthT2 {0.2};
+    double linkLengthT3 {0.2};
+    double linkLengthT4 {0.1};
 
     // twist angle for t1...t4
-    double twistAngleT1 {90};
+    double twistAngleT1 {0};
     double twistAngleT2 {0};
     double twistAngleT3 {0};
     double twistAngleT4 {0};
 
     //joint offset for t1...t4
-    double jointOffsetT1 {0.077};
-    double jointOffsetT2 {0};
+    double jointOffsetT1 {0};
+    double jointOffsetT2 {0.1};
     double jointOffsetT3 {0};
     double jointOffsetT4 {0};
 
     //joint angle for t1...t4
-    double jointAngleT1 {103};
-    double jointAngleT2 {15};
-    double jointAngleT3 {-5};
-    double jointAngleT4 {21};
+    double jointAngleT1 {30};
+    double jointAngleT2 {-60};
+    double jointAngleT3 {45};
+    double jointAngleT4 {-15};
 
     T1<<cos(jointAngleT1), -sin(jointAngleT1) * cos(twistAngleT1), sin(jointAngleT1) * sin(twistAngleT1) , linkLengthT1 * cos(jointAngleT1),
         sin(jointAngleT1), cos(jointAngleT1) * cos(twistAngleT1), -cos(jointAngleT1) * sin(twistAngleT1), linkLengthT1 * sin(jointAngleT1),
         0, sin(twistAngleT1), cos(twistAngleT1), jointOffsetT1,
         0,0,0,1;
 
+    cout<<T1<<endl;
 
     T2<<cos(jointAngleT2), -sin(jointAngleT2) * cos(twistAngleT2), sin(jointAngleT2) * sin(twistAngleT2) , linkLengthT2 * cos(jointAngleT2),
         sin(jointAngleT2), cos(jointAngleT2) * cos(twistAngleT2), -cos(jointAngleT2) * sin(twistAngleT2), linkLengthT2 * sin(jointAngleT2),
         0, sin(twistAngleT2), cos(twistAngleT2), jointOffsetT2,
         0,0,0,1;
 
+    cout<<T2<<endl;
+
     T3<<cos(jointAngleT3), -sin(jointAngleT3) * cos(twistAngleT3), sin(jointAngleT3) * sin(twistAngleT3) , linkLengthT3 * cos(jointAngleT3),
         sin(jointAngleT3), cos(jointAngleT3) * cos(twistAngleT3), -cos(jointAngleT3) * sin(twistAngleT3), linkLengthT3 * sin(jointAngleT3),
         0, sin(twistAngleT3), cos(twistAngleT3), jointOffsetT3,
         0,0,0,1;        
 
+    cout<<T3<<endl;
 
     T4<<cos(jointAngleT4), -sin(jointAngleT4) * cos(twistAngleT4), sin(jointAngleT4) * sin(twistAngleT4) , linkLengthT4 * cos(jointAngleT4),
         sin(jointAngleT4), cos(jointAngleT4) * cos(twistAngleT4), -cos(jointAngleT4) * sin(twistAngleT4), linkLengthT4 * sin(jointAngleT4),
         0, sin(twistAngleT4), cos(twistAngleT4), jointOffsetT4,
         0,0,0,1;
 
+    cout<<T4<<endl;
+
     T = T1 * T2 * T3 * T4;
 
 
     cout<<T<<endl;
     cout<<"End effector orientation: \n"<<T.block(0,0,3,3)<<"\n";
-    //cout<<"End Effector Position: \n"<<T.block(0,4,3,1)<<"\n";
+    cout<<"End Effector Position: \n"<<T.block(0,3,3,1)<<"\n";
     
     return 0;
 }
