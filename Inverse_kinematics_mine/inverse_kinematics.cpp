b7d96375870217d5f481aaa30d28d4ba0e8bf093
Manasseh <kimmanasseh14@gmail.com>
2023-03-29 23:33:01 +0300

    final commit

--- Inverse_kinematics_mine/inverse_kinematics.cpp
+++ Inverse_kinematics_mine/inverse_kinematics.cpp
@@ -1,9 +1,7 @@
 /* this is for a robot with a DoF of 3 */
 #include <iostream>
-#include <Eigen/Dense>
 #include <cmath>
 
-using namespace Eigen;
 using std::cout;
 using std::endl;
 
@@ -28,7 +26,7 @@ int main()
     
     c2 = (pow(x, 2) + pow(y, 2) - pow(L1, 2) - pow(L2, 2)) / (2 * L1 *L2);  
 
-    if (c2 > -1 && c2 < 1){
+    if (c2 > -1 && c2 < 1){ // compute for error
         s2_1 = +sqrt(1 - pow(c2, 2));
         s2_2 = -sqrt(1 - pow(c2, 2));
 
@@ -44,6 +42,8 @@ int main()
     double theta3_1 {};
     double theta3_2 {};
 
+    
+
     //the two values for theta2
     theta2_1 = atan2(s2_1, c2);
     theta2_2 = atan2(s2_2, c2);
@@ -52,9 +52,13 @@ int main()
     theta1_1 = (atan2(y, x) - atan2(L2 * s2_1, L1 + L2 * c2));
     theta1_2 = (atan2(y, x) - atan2(L2 * s2_2, L1 + L2 * c2));
 
+    //angles for phi
+    double sin_phi = sin(phi);
+    double cos_phi = cos(phi);
+
     //the two values for theta 3
-    theta3_1 = (atan2(sin(phi), cos(phi)) - theta1_1 - theta2_1);
-    theta3_2 = (atan2(sin(phi), cos(phi)) - theta1_2 - theta2_2);
+    theta3_1 = atan2(sin_phi, cos_phi) - theta1_1 - theta2_1;
+    theta3_2 = atan2(sin_phi, cos_phi) - theta1_2 - theta2_2;
 
     cout<<"the joint values 1 are: "<<theta1_1<<", "<<theta2_1<<", "<<theta3_1<<" radians."<<endl;
     cout<<"the joint values 2 are: "<<theta1_2<<", "<<theta2_2<<", "<<theta3_2<<" radians."<<endl;
---
+++ build/CMakeFiles/test_regression.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_regression.dir/Inverse_kinematics_mine/inverse_kinematics.cpp.o"
+  "CMakeFiles/test_regression.dir/Inverse_kinematics_mine/inverse_kinematics.cpp.o.d"
+  "CMakeFiles/test_regression.dir/forward_kinematics_mine/forward_kinematics.cpp.o"
+  "CMakeFiles/test_regression.dir/forward_kinematics_mine/forward_kinematics.cpp.o.d"
+  "test_regression"
+  "test_regression.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_regression.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
