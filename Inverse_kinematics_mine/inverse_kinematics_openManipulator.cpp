becfb5093be3c38f7f21c59edc40b5cccab72dc9
Manasseh <kimmanasseh14@gmail.com>
2023-04-02 12:31:47 +0300

    add code for computing theta1...theta4

--- Inverse_kinematics_mine/inverse_kinematics_openManipulator.cpp
+++ Inverse_kinematics_mine/inverse_kinematics_openManipulator.cpp
@@ -8,10 +8,13 @@ using namespace Eigen;
 
 int main()
 {
+    const double  degree_conversion {(180/3.1415)};
+    const double radians_conversion {(3.1415/180)};
+
     //end effector position
-    double x {-0.0833};
-    double y {0.361};
-    double z {0.1744};
+   const double x {-0.0833};
+   const double y {0.361};
+   const double z {0.1744};
 
     // link lengths
     double l1 {0};
@@ -41,48 +44,65 @@ int main()
     double theta4_2 {};
 
     //end effector angle relative to base frame
-    double phi{15-5+21};
+    double phi{(15-5+21) * radians_conversion};
+    double theta_prep {atan(y/x) * degree_conversion}; // find theta 1
+    
 
-    theta1 = atan2(y, x);
+    if (x > 0 && y > 0){
+      theta1 = theta_prep;  
+    }else if (x < 0 && y > 0){
+        theta1 = theta_prep + 180;
+    }else if(x < 0 && y <0){
+        theta1 = theta_prep - 180;
+    }else if(x > 0 && y == 1){
+        theta1 = theta_prep;
+    }
 
     double pr {sqrt(pow(x, 2)+ pow(y,2))};
     
+
     double r3 {pr};
 
     double z3 {z -d1};
 
-    double r2 {r3 - l4 * cos(phi)};
-    double z2 {z3 - l4 * sin(phi)};
+    double r2 {(r3 - (l4 * cos(phi)))};
+    double z2 {(z3 - (l4 * sin(phi)))};
+    // cout<<r2<<endl;
+    // cout<<z2<<endl;
+    // cout<<cos(phi)<<endl;
 
-    theta3_1 = +acos((pow(r2, 2) + pow(z2, 2) - (pow(l2, 2) + pow(l3, 2)))/(2 * l2 * a3));
-    theta3_2 = -acos((pow(r2, 2) + pow(z2, 2) - (pow(l2, 2) + pow(l3, 2)))/(2 * l2 * a3));
+    double conversion_acos {(((r2 * r2) + (z2 * z2)) - ((l2 * l2) + (l3 * l3)))/(2 * l2 * l3)};
+
+    theta3_1 = +acos(conversion_acos) * degree_conversion;
+    theta3_2 = -acos(conversion_acos) * degree_conversion;
     
     double x_1 {}; //cos(theta_2_1)
-    x_1 = ((l2 + l3 * cos(theta3_1)) * r2 + (l3 * sin(theta3_1)) * z2) / (pow(r2, 2) + pow(z2, 2));
+    x_1 = ((l2 + l3 * cos((theta3_1 * radians_conversion))) * r2 + (l3 * sin((theta3_1 * radians_conversion))) * z2) / ((r2, r2) + (z2, r2));
+    cout<<x_1<<endl;
 
     double x_2 {}; //cos(theta_2_2)
-    x_2 = ((l2 + l3 * cos(theta3_2)) * r2 + (l3 * sin(theta3_2)) * z2) / (pow(r2, 2) + pow(z2, 2));
+    x_2 = ((l2 + l3 * cos((theta3_2 * radians_conversion))) * r2 + (l3 * sin((theta3_2 * radians_conversion))) * z2) / ((r2, r2) + (z2, r2));
+    cout<<x_2<<endl;
 
     double y_1 {}; //sin(theta_2_1)
-    y_1 = ((l2 + l3 * cos(theta3_1)) * z2 + (l3 * sin(theta3_1)) * r2) / (pow(r2, 2) + pow(z2, 2));
+    y_1 = ((l2 + l3 * cos((theta3_1 * radians_conversion))) * z2 + (l3 * sin((theta3_1 * radians_conversion))) * r2) / ((r2 * r2) + (z2 * z2));
 
     double y_2 {}; //sin(theta _2_1)
-    y_2 = ((l2 + l3 * cos(theta3_2)) * z2 + (l3 * sin(theta3_2)) * r2) / (pow(r2, 2) + pow(z2, 2));
+    y_2 = ((l2 + l3 * cos((theta3_2 * radians_conversion))) * z2 + (l3 * sin((theta3_2 * radians_conversion))) * r2) / ((r2 * r2) + (z2 * z2));
 
-    theta2_1 = atan(sin(theta2_1)/ cos(theta2_1));
-    theta2_2 = atan(sin(theta2_2)/ cos(theta2_2));
+    theta2_1 = (atan(sin(y_1)/ cos(x_1)) * degree_conversion);
+    theta2_2 = (atan(sin(y_2)/ cos(x_2)) * degree_conversion);
 
     theta4_1  = phi - (theta2_1 + theta3_1);
     theta4_2  = phi - (theta2_2 + theta3_2);
 
-    const double pi{3.142};
-    cout<<"Theta 1: "<<theta1 * (180/pi)<<endl;
-    cout<<"Theta 2_1: "<<theta2_1 * (180/pi)<<endl;
-    cout<<"Theta 2_2: "<<theta2_2 * (180/pi)<<endl;
-    cout<<"Theta 3_1: "<<theta3_1 * (180/pi)<<endl;
-    cout<<"Theta 3_2: "<<theta3_2 * (180/pi)<<endl;
-    cout<<"Theta 4_1: "<<theta4_1 * (180/pi)<<endl;
-    cout<<"Theta 4_2: "<<theta4_2 * (180/pi)<<endl;
+    cout<<"Theta 1: "<<theta1<<endl;
+    cout<<"Theta 2_1: "<<theta2_1<<endl;
+    cout<<"Theta 2_2: "<<theta2_2<<endl;
+    cout<<"Theta 3_1: "<<theta3_1<<endl;
+    cout<<"Theta 3_2: "<<theta3_2<<endl;
+    cout<<"Theta 4_1: "<<theta4_1<<endl;
+    cout<<"Theta 4_2: "<<theta4_2<<endl;
 
     return 0;
 }
